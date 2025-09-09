b096044733689ed7fd5916d3145717c59a0b0d58
Elizabeth Duarte Ortiz <134437416+liv-ortiz25@users.noreply.github.com>
2025-09-09 01:37:06 +0000

    trabajo terminado

---
+++ src/js/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer (){
+  return(
+        <nav className="navbar navbar-expand-xl text-white bg-dark justify-content-center">
+      <p className="text-center">Copyright © Your Website 2023.</p>
+        </nav>
+    )
+};
+
+export default Footer;
